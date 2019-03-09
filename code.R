d00c54345f9f88514e5a77be52c0147156253c78
abramovichr <reuven.abramovich123@gmail.com>
2019-03-10 01:31:38 +0200

    We've added a vector just to verify the process baby

---
+++ code.R
@@ -0,0 +1,9 @@
+
+### to Start you need to activate ggplot2 and dplyr for the filter command
+
+### scatter graph in R. 
+
+ggplot(data = mpg) + 
+  geom_point(mapping = aes(x = displ, y = hwy))
+
+  
